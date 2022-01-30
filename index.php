9c908e145aa22d40bf02c46d9ae29b8dccfa532f
LegendaNiks <98683563+LegendaNiks@users.noreply.github.com>
2022-01-30 10:35:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8"/>
+<link href="styles/css/style.css" rel="stylesheet"/>
+</head>
+
+<body>
+<h1 class="room">Online_Chat</h1>
+<iframe src="chat.html" class="cec" height="350px" width="650px"></iframe> </br>
+</br>
+<strong class="cec1">Написать в чат</strong> </br>
+</br>
+<form action="send.php" method="post">
+<input value="Имя" name="name"/> </br>
+</br> 
+<input size="70" name="msg"/>
+<button type="submit">Отправить</button>
+</form>
+</body>
+
+
+</html>
